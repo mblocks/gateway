ba2a455edb598841c1e5b9b32d03c486aa4c4f3a
jooonwood <jooonwood@gmail.com>
2022-03-05 17:20:40 +0800

    Update download.sh

--- scripts/download.sh
+++ scripts/download.sh
@@ -2,4 +2,5 @@
 cd /usr/local/openresty/nginx/html
 
 wget $(wget https://api.github.com/repos/$1/releases/latest -q -O - | grep 'dist.zip' | cut -d\" -f4  | cut -d$'\n' -f2) -O $2.zip && unzip $2.zip -d /usr/local/openresty/nginx/html/$2
+mv /usr/local/openresty/nginx/html/$2/dist/* /usr/local/openresty/nginx/html/$2
 rm $2.zip
